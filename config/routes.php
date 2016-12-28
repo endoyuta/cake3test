d29c4727c57a9a51c0068c45f61c19eb86551718
edo <edo96815@gmail.com>
2016-12-29 06:46:36 +0900

    Itemsテーブル作成してBakeした。 config/routes.phpを修正してjsonで取得できるようにした。

--- config/routes.php
+++ config/routes.php
@@ -44,12 +44,8 @@ use Cake\Routing\Route\DashedRoute;
 Router::defaultRouteClass(DashedRoute::class);
 
 Router::scope('/', function (RouteBuilder $routes) {
-    /**
-     * Here, we are connecting '/' (base path) to a controller called 'Pages',
-     * its action called 'display', and we pass a param to select the view file
-     * to use (in this case, src/Template/Pages/home.ctp)...
-     */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->extensions(['json']);
+    $routes->connect('/', ['controller' => 'Items', 'action' => 'index']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
